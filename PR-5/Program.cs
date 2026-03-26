8708c0dcf6479d950222ecd9f7523c91793b18b0
Bhautik Ranpara <ranparabhautik30@gmail.com>
2026-03-26 18:29:02 +0530

    Adding Practical 5

---
+++ PR-5/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 10, 50, 60, 30, 80 };
+
+        try
+        {
+            for (int i = 0; i < 6; i++) 
+            {
+                Console.WriteLine($"{i} -> {arr[i]}");
+            }
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("IndexOutOfRangeException: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("General Exception: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Finally Block Executed");
+        }
+    }
+}
